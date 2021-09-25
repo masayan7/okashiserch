3c8f036b9f87deed2c0f8b3ddda134d9cfcdc31c
masayan7 <masaya.ishigami.5.8@gmail.com>
2021-09-25 14:15:20 +0900

    Initial Commit

---
+++ okashi/okashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  okashiApp.swift
+//  okashi
+//
+//  Created by masaya ishigami on 2021/09/25.
+//
+
+import SwiftUI
+
+@main
+struct okashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
